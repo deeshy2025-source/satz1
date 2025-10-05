a4c40d13544b774753ff6a3bae137211249c1ba1
deeshy2025 <deeshy2025@gmail.com>
2025-10-05 13:51:42 +0300

    make hover card

--- lib/presentation/widgets/ourexpertize/expertise_card.dart
+++ lib/presentation/widgets/ourexpertize/expertise_card.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:satz1/core/classes/expertise_data.dart';
 import 'package:satz1/core/constants/const_text.dart';
+import 'package:satz1/presentation/widgets/hover_card.dart';
 
 class ExpertiseCard extends StatelessWidget {
 
@@ -12,38 +13,40 @@ class ExpertiseCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints: const BoxConstraints(minHeight: 400),
-      padding: const EdgeInsets.all(32),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(60),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 12,
-            offset: Offset(0, 6),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SelectableText(expertiseCard.title,
-              style: ConstText.TextMid(context)),
-          const SizedBox(height: 12),
-          for (final p in expertiseCard.points)
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 3),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SelectableText("• "),
-                  Expanded(child: SelectableText(p,style: ConstText.body(context),)),
-                ],
-              ),
+    return HoverCard(
+      child: Container(
+        constraints: const BoxConstraints(minHeight: 400),
+        padding: const EdgeInsets.all(32),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(60),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 12,
+              offset: Offset(0, 6),
             ),
-        ],
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SelectableText(expertiseCard.title,
+                style: ConstText.TextMid(context)),
+            const SizedBox(height: 12),
+            for (final p in expertiseCard.points)
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 3),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SelectableText("• "),
+                    Expanded(child: SelectableText(p,style: ConstText.body(context),)),
+                  ],
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
