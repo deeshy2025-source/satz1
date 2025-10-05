6b9751093438fb287be01413cbec06a087a2d3fc
deeshy2025 <deeshy2025@gmail.com>
2025-10-05 16:19:29 +0300

    make hover card3

--- lib/presentation/widgets/hover_card.dart
+++ lib/presentation/widgets/hover_card.dart
@@ -4,14 +4,12 @@ class HoverCard extends StatefulWidget {
   final Widget child;
   final double scale;
   final Duration duration;
-  final bool addShadow;
 
   const HoverCard({
     super.key,
     required this.child,
     this.scale = 1.05,
     this.duration = const Duration(milliseconds: 200),
-    this.addShadow = false, // الافتراضي بدون ظل
   });
 
   @override
@@ -21,33 +19,30 @@ class HoverCard extends StatefulWidget {
 class _HoverCardState extends State<HoverCard> {
   bool _isHovered = false;
 
+  void _toggleHover(bool value) {
+    if (mounted) {
+      setState(() => _isHovered = value);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MouseRegion(
-      onEnter: (_) => setState(() => _isHovered = true),
-      onExit: (_) => setState(() => _isHovered = false),
-      child: AnimatedScale(
-        scale: _isHovered ? widget.scale : 1.0,
-        duration: widget.duration,
-        curve: Curves.easeOut,
-        child: AnimatedContainer(
+      onEnter: (_) => _toggleHover(true),
+      onExit: (_) => _toggleHover(false),
+      child: GestureDetector(
+        onTapDown: (_) => _toggleHover(true),   // عند الضغط
+        onTapUp: (_) => _toggleHover(false),    // عند الإفلات
+        onTapCancel: () => _toggleHover(false), // لو لمسة اتلغت
+        child: AnimatedScale(
+          scale: _isHovered ? widget.scale : 1.0,
           duration: widget.duration,
           curve: Curves.easeOut,
-          // 👇 هنا بقى مفيش خلفية ولا curve ظاهر
-          decoration: widget.addShadow && _isHovered
-              ? BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.1),
-                blurRadius: 20,
-                offset: const Offset(0, 10),
-              ),
-            ],
-          )
-              : const BoxDecoration(),
-          // 👇 نلغي أي clipping عشان نحافظ على شكل الchild
-          clipBehavior: Clip.none,
-          child: widget.child,
+          child: AnimatedContainer(
+            duration: widget.duration,
+            curve: Curves.easeOut,
+            child: widget.child,
+          ),
         ),
       ),
     );
